fb1c344b2b7520d529cda84029d68885667cd06c
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2026-02-07 21:29:22 +0000

    add linux server config and remove unused files

--- windows/script/shell/utils.ps1
+++
@@ -1,6 +0,0 @@
-Function Write-Log {
-    Param ([string]$message)
-    $logfile = "$env:USERPROFILE\Documents\scripts.log"
-    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    "$timestamp - $message" | Out-File -Append -FilePath $logfile
-}
